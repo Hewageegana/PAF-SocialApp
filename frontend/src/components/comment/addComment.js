08031b4367f523287e58b31f8947e1a849de7566
Hewageegana <rusiru2000hewageegana@gmail.com>
2023-05-04 01:14:41 +0530

    xdf

--- frontend/src/components/comment/addComment.js
+++ frontend/src/components/comment/addComment.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import swal from "sweetalert";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/login.css';
 import '../css/post.css';
@@ -10,6 +11,49 @@ import SendIcon from '@mui/icons-material/Send';
 
 
 export default function AddComment() {
+
+    const [commentText, setCommentText] = useState("");
+
+    function sendData(e) {
+        const newComment = {
+            commentText
+        };
+        axios
+            .post("social-media-domain/users/304cccfe-a431-4330-810c-2fd288346dab/posts/b1f84bf0-5227-4d6d-b2bc-8c75debbc871/comments", newComment)
+            .then(() => {
+                swal({
+                    title: "Success!",
+                    text: "Successfully added the room",
+                    icon: "success",
+                    button: "Ok",
+                });
+
+            })
+            .catch((e) => {
+                swal("Please fill Form correctly" + e);
+            });
+    }
+
+    const [post, setPost] = useState([]);
+    const [postList, setPostList] = useState([]);
+
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const res = await axios
+                .get("/social-media-domain/users/304cccfe-a431-4330-810c-2fd288346dab/posts")
+                .then((res) => {
+                    setPostList(res.data);
+                    res.data.map(item => {
+                        console.log(item);
+                    })
+
+                })
+                .catch(() => { });
+        };
+        getPosts();
+    }, []);
+    
     return (
         <>
             <div className="comment-container">
@@ -35,14 +79,16 @@ export default function AddComment() {
                 </div>
                 <div className="comment-field">
                     <div className="">
-                        <input type="text" class="comment-input" id="exampleFormControlInput1" placeholder="type ur comment here....." />
+                        <input type="text" class="comment-input" id="exampleFormControlInput1" placeholder="type ur comment here....." onChange={(e) => {
+                            setCommentText(e.target.value);
+                        }} />
                     </div>
                     <div>
-                        &nbsp;&nbsp;&nbsp;&nbsp; <button><SendIcon /></button>
+                        &nbsp;&nbsp;&nbsp;&nbsp; <button type="submit" onClick={sendData()}><SendIcon /></button>
                     </div>
                 </div>
 
-                <span className="op">faadddddddddddddddddv</span>
+                <span className="op">hidden text</span>
             </div>
         </>
     );
--- frontend/src/components/status/createStatus.js
+++ frontend/src/components/status/createStatus.js
@@ -4,9 +4,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/login.css';
 import '../css/post.css';
 import loginimg from '../../Assets/images/photo1.jpeg'
+import '../css/status.css'
 
 
 export default function CreateStatus() {
+    const [post, setPost] = useState([]);
+    const [statustList, setStatusList] = useState([]);
+
+
+    useEffect(() => {
+        const getStatus = async () => {
+            const res = await axios
+                .get("/social-media-domain/users/304cccfe-a431-4330-810c-2fd288346dab/status")
+                .then((res) => {
+                    setStatusList(res.data);
+                    res.data.map(item => {
+                        console.log(item);
+                    })
+
+                })
+                .catch(() => { });
+        };
+        getStatus();
+    }, []);
+
     return (
         <>
             <div className="container">
@@ -17,36 +38,11 @@ export default function CreateStatus() {
                             <div className="card-body">
                                 <h5 className="card-title">Card title</h5>
                                 <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <button type="button" class="btn btn-danger">DELETE</button>
                             </div>
                         </div>
                     </div>
-                    <div className="col">
-                        <div className="card">
-                            <img src="..." className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card">
-                            <img src="..." className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card">
-                            <img src="..." className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                    </div>
+
                 </div>
             </div>
         </>
